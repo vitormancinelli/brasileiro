c1fc7198858d3d0d96062c4bce73a689bfb6bdac
vitor <vitor@DESKTOP-759UAEO>
2020-06-16 20:02:53 -0300

    arrumando menu

--- Brasileirao/src/servico/Brasileirao.java
+++ Brasileirao/src/servico/Brasileirao.java
@@ -6,15 +6,16 @@ public class Brasileirao {
 	public static void menu() {
 		System.out.println("Inicio do programa");
 		System.out.println("1. Cadastrar Campeonato");
-		System.out.println("2. Limpar Campeonato");
-		System.out.println("3. Cadastrar Time");
-		System.out.println("4. Limpar Time");
-		System.out.println("5. Sortear Times");
-		System.out.println("6. Sortear Resultados");
-		System.out.println("7. Listar Partidas");
-		System.out.println("8. Ver tabela com Resultados");
-		System.out.println("9. Ver vencedores");
-		System.out.println("10. Fim");
+		System.out.println("2. Listar Campeonato");
+		System.out.println("3. Limpar Campeonato");
+		System.out.println("4. Cadastrar Time");
+		System.out.println("5. Limpar Time");
+		System.out.println("6. Sortear Times");
+		System.out.println("7. Sortear Resultados");
+		System.out.println("8. Listar Partidas");
+		System.out.println("9. Ver tabela com Resultados");
+		System.out.println("10. Ver vencedores");
+		System.out.println("11. Fim");
 	}
 
 	public static void main(String[] Args) {
@@ -36,51 +37,57 @@ public class Brasileirao {
 				bancoDeDados.cadastrarCampeonato(2, 19, 10, 20);
 				bancoDeDados.desconectar();
 				break;
-
+				
 			case 2:
-				bancoDeDados.limparCampeonato();
+				bancoDeDados.listarCampeonatos();
 				bancoDeDados.desconectar();
 				break;
 
 			case 3:
+				bancoDeDados.limparCampeonato();
+				bancoDeDados.desconectar();
+				break;
+
+			case 4:
 				System.out.println("digite o nome do time a ser incluido");
 				b = e2.nextLine();
 				bancoDeDados.cadastrarTime(b);
 				bancoDeDados.desconectar();
 				break;
 
-			case 4:
+			case 5:
 				System.out.println("digite o numero do time a ser excluido");
 				a = e1.nextInt();
 				bancoDeDados.limparTime(a);
 				bancoDeDados.desconectar();
 				break;
 
-			case 5:
+			case 6:
 				bancoDeDados.sortearTimes();
 				bancoDeDados.desconectar();
 				break;
 
-			case 6:
+			case 7:
 				bancoDeDados.sortearResultados();
 				bancoDeDados.desconectar();
 				break;
 				
-			case 7:
+			case 8:
 				bancoDeDados.listarPartidas();
 				bancoDeDados.desconectar();
 				break;
 
-			case 8:
+			case 9:
 				bancoDeDados.mostrarClassificaçao();
 				bancoDeDados.desconectar();
 				break;
 			
-			case 9:
+			case 10:
 				bancoDeDados.mostrarVencedores();
 				bancoDeDados.desconectar();
-
-			case 10:
+				break;
+				
+			case 11:
 				System.out.println("Encerado");
 				System.exit(0);
 
